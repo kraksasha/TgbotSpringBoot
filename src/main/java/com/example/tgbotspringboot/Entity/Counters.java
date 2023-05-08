e9cb09e402a11931312aee7905ee65c46dca8c79
kraksasha <kraksasha@yandex.ru>
2023-05-08 20:36:37 +0300

    Отображение откликов (дополнен)

---
+++ src/main/java/com/example/tgbotspringboot/Entity/Counters.java
@@ -0,0 +1,14 @@
+package com.example.tgbotspringboot.Entity;
+
+public class Counters {
+
+    private int responses;
+
+    public int getResponses() {
+        return responses;
+    }
+
+    public void setResponses(int responses) {
+        this.responses = responses;
+    }
+}
